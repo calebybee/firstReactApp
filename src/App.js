9f3f3abb48e8f84930036d00c48b8f883a17bc7a
Cale Bybee <bybeecale@gmail.com>
2018-11-12 12:51:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'reader'
+    }
+  }
+  handleNameChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      name: value
+    });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type={'test'} 
+          placeholder={'Your name here'} 
+          value={this.state.name} 
+          onChange={this.handleNameChange}
+        />
+        <h1>Hello, {this.state.name}!</h1>
       </div>
     );
   }
